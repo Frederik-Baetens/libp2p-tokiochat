c1682a00bb2f41597666b871d1dea496a4665597
Frederik Baetens <baetens.fr@gmail.com>
2021-12-20 11:37:32 +0100

    add explicit do_px to gossipsubconfig

--- src/main.rs
+++ src/main.rs
@@ -101,6 +101,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
             .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
             .message_id_fn(message_id_fn) // content-address messages. No two messages of the
+            .do_px()
             // same content will be propagated.
             .build()
             .expect("Valid config");
