714a8a0191455da39607b25ccdea5b4993e45a9b
Frederik Baetens <baetens.fr@gmail.com>
2022-01-29 18:38:31 +0100

    add peer discovery based on kademlia and identify

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 Parity Technologies (UK) Ltd.
+// Copyright 2016 Parity Technologies (UK) Ltd.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -18,71 +18,140 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-//! A basic chat application demonstrating libp2p with the mDNS and gossipsub protocols
-//! using tokio for all asynchronous tasks and I/O. In order for all used libp2p
+//! A basic chat application demonstrating libp2p with the gossipsub protocol, supported by
+//! the kademlia and identify protocols for peer discovery.
+//! This example uses tokio for all asynchronous tasks and I/O. In order for all used libp2p
 //! crates to use tokio, it enables tokio-specific features for some crates.
 //!
 //! The example is run per node as follows:
 //!
 //! ```sh
-//! cargo run --example chat-tokio --features="tcp-tokio mdns-tokio"
-//! ```
-//!
-//! Alternatively, to run with the minimal set of features and crates:
-//!
-//! ```sh
-//!cargo run --example chat-tokio \\
-//!    --no-default-features \\
-//!    --features="gossipsub mplex noise tcp-tokio mdns-tokio"
+//! cargo run --example peer-discovery --features="tcp-tokio mdns-tokio"
 //! ```
 
+use futures::StreamExt;
 use libp2p::{
+    core::{upgrade, PublicKey},
     gossipsub::{
         self, Gossipsub, GossipsubEvent, GossipsubMessage, IdentTopic, MessageAuthenticity,
         MessageId, ValidationMode,
     },
-    Transport,
-    core::upgrade,
-    identity,
-    noise,
-    mplex,
+    identify::{Identify, IdentifyConfig, IdentifyEvent, IdentifyInfo},
+    identity::{self, Keypair},
+    kad::{
+        self, record::store::MemoryStore, GetClosestPeersError, GetClosestPeersOk, Kademlia,
+        KademliaConfig, KademliaEvent, QueryResult,
+    },
+    mplex, noise,
+    swarm::NetworkBehaviourEventProcess,
     tcp::TokioTcpConfig,
-    Multiaddr,
-    PeerId,
-    Swarm,
-    NetworkBehaviour,
-    swarm::{SwarmEvent, NetworkBehaviourEventProcess},
+    Multiaddr, NetworkBehaviour, PeerId, Swarm, Transport,
 };
 use std::collections::hash_map::DefaultHasher;
-use std::error::Error;
 use std::hash::{Hash, Hasher};
-use std::time::Duration;
-use futures::StreamExt;
+use std::{env::args, error::Error, time::Duration};
 use tokio::io::{self, AsyncBufReadExt};
 
 #[derive(NetworkBehaviour)]
 #[behaviour(event_process = true)]
 struct ChatBehaviour {
     gossipsub: Gossipsub,
+    kademlia: Kademlia<MemoryStore>,
+    identify: Identify,
 }
 
 impl NetworkBehaviourEventProcess<GossipsubEvent> for ChatBehaviour {
     fn inject_event(&mut self, event: GossipsubEvent) {
         if let GossipsubEvent::Message {
-                propagation_source: peer_id,
-                message_id: id,
-                message,
-            } = event {
-                println!(
-                    "Got message: {} with id: {} from peer: {:?}",
-                    String::from_utf8_lossy(&message.data),
-                    id,
-                    peer_id
-                    );
-            }
+            propagation_source: peer_id,
+            message_id: id,
+            message,
+        } = event
+        {
+            println!(
+                "Got message: {} with id: {} from peer: {:?}",
+                String::from_utf8_lossy(&message.data),
+                id,
+                peer_id
+            );
+        }
     }
 }
 
+impl NetworkBehaviourEventProcess<IdentifyEvent> for ChatBehaviour {
+    fn inject_event(&mut self, event: IdentifyEvent) {
+        match event {
+            IdentifyEvent::Received {
+                peer_id,
+                info:
+                    IdentifyInfo {
+                        listen_addrs,
+                        protocols,
+                        ..
+                    },
+            } => {
+                if protocols
+                    .iter()
+                    .any(|p| p.as_bytes() == kad::protocol::DEFAULT_PROTO_NAME)
+                {
+                    for addr in listen_addrs {
+                        println!("received addr {addr} trough identify");
+                        self.kademlia.add_address(&peer_id, addr);
+                    }
+                } else {
+                    println!("some funky shit happened, investigate it");
+                }
+            }
+            e => {
+                println!("got other identify event");
+                dbg!(e);
+            }
+        }
+    }
+}
+
+impl NetworkBehaviourEventProcess<KademliaEvent> for ChatBehaviour {
+    fn inject_event(&mut self, event: KademliaEvent) {
+        match event {
+            KademliaEvent::OutboundQueryCompleted {
+                result: QueryResult::GetClosestPeers(result),
+                ..
+            } => {
+                match result {
+                    Ok(GetClosestPeersOk { key: _, peers }) => {
+                        if !peers.is_empty() {
+                            println!("Query finished with closest peers: {:#?}", peers);
+                            for peer in peers {
+                                println!("gossipsub adding peer {peer}");
+                                self.gossipsub.add_explicit_peer(&peer);
+                            }
+                        } else {
+                            // The example is considered failed as there
+                            // should always be at least 1 reachable peer.
+                            println!("Query finished with no closest peers.")
+                        }
+                    }
+                    Err(GetClosestPeersError::Timeout { peers, .. }) => {
+                        if !peers.is_empty() {
+                            println!("Query timed out with closest peers: {:#?}", peers);
+                            for peer in peers {
+                                println!("gossipsub adding peer {peer}");
+                                self.gossipsub.add_explicit_peer(&peer);
+                            }
+                        } else {
+                            // The example is considered failed as there
+                            // should always be at least 1 reachable peer.
+                            println!("Query timed out with no closest peers.");
+                        }
+                    }
+                };
+            }
+            evnt => {
+                dbg!(evnt);
+            }
+        }
+    }
+}
 
 /// The `tokio::main` attribute sets up a tokio runtime.
 #[tokio::main]
@@ -111,28 +180,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Create a Gossipsub topic
     let gossipsub_topic = IdentTopic::new("chat");
 
+    // get optional boot address and peerId
+    let boot_addr: Option<Multiaddr> = args().nth(1).map(|a| a.parse().unwrap());
+    let boot_peerid: Option<PeerId> = args().nth(2).map(|a| a.parse().unwrap());
+
     // Create a Swarm to manage peers and events.
     let mut swarm: Swarm<ChatBehaviour> = {
-        // To content-address message, we can take the hash of message and use it as an ID.
-        let message_id_fn = |message: &GossipsubMessage| {
-            let mut s = DefaultHasher::new();
-            message.data.hash(&mut s);
-            MessageId::from(s.finish().to_string())
-        };
-
-        // Set a custom gossipsub
-        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
-            .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-            .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-            .message_id_fn(message_id_fn) // content-address messages. No two messages of the
-            .do_px()
-            // same content will be propagated.
-            .build()
-            .expect("Valid config");
-
         let mut chatbehaviour = ChatBehaviour {
-            gossipsub: gossipsub::Gossipsub::new(MessageAuthenticity::Signed(id_keys), gossipsub_config)
-                .expect("Correct configuration"),
+            gossipsub: create_gossipsub_behavior(id_keys.clone()),
+            kademlia: create_kademlia_behavior(peer_id),
+            identify: create_identify_behavior(id_keys.public()),
         };
 
         // subscribes to our topic
@@ -147,31 +204,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .build()
     };
 
-    // Reach out to another node if specified
-    if let Some(to_dial) = std::env::args().nth(1) {
-        println!("dialing {}", to_dial);
-        let addr: Multiaddr = to_dial.parse()?;
-        swarm.dial(addr)?;
-        println!("Dialed {:?}", to_dial)
-    }
-
     // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?).unwrap();
 
-    use futures::future;
-    use futures::Stream;
-    use std::task::Poll;
-    use std::pin::Pin;
-    future::poll_fn(|cx| {
-        if swarm.listeners().peekable().peek().is_some() {
-            Poll::Ready(None)
-        } else {
-            Pin::new(&mut swarm).poll_next(cx)
-        }
-    }).await;
-    for addr in swarm.listeners() {
-        dbg!(addr);
+    // Reach out to another node if specified
+    if let Some(boot_addr) = boot_addr {
+        println!("dialing {}", &boot_addr);
+        swarm.dial(boot_addr.clone())?;
+        println!("Dialed {:?}", &boot_addr);
+
+        println!("pushing identity to boot node");
+        swarm
+            .behaviour_mut()
+            .identify
+            .push(std::iter::once(boot_peerid.unwrap()));
+
+        println!("pushing adding boot node to kademlia routing table");
+        swarm
+            .behaviour_mut()
+            .kademlia
+            .add_address(&boot_peerid.unwrap(), boot_addr);
     }
+
+    // Order Kademlia to search for a peer.
+    let to_search: PeerId = identity::Keypair::generate_ed25519().public().into();
+    println!("Searching for the closest peers to {:?}", to_search);
+    swarm.behaviour_mut().kademlia.get_closest_peers(to_search);
+
     tokio::spawn(run(swarm, gossipsub_topic)).await.unwrap();
     Ok(())
 }
@@ -179,14 +238,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
 async fn run(mut swarm: Swarm<ChatBehaviour>, gossipsub_topic: IdentTopic) {
     // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines();
-    //let peerid: PeerId = "12D3KooWK379MZgvrnYLYj2zFDuVWRLZbF7JBsrNqrJYJqXvgfzv".parse().unwrap();
-    //swarm.gossipsub.add_explicit_peer(&peerid);
     // Kick it off
     let mut listening = false;
     loop {
         if !listening {
             for addr in swarm.listeners() {
                 println!("Listening on {:?}", addr);
+                let peer_id = swarm.local_peer_id();
+                println!("connect additional nodes with \"cargo run -- {addr} {peer_id}\"");
                 listening = true;
             }
         }
@@ -202,19 +261,55 @@ async fn run(mut swarm: Swarm<ChatBehaviour>, gossipsub_topic: IdentTopic) {
                         None
                     }
                 }
-                event = swarm.select_next_some() => {
+                _ = swarm.select_next_some() => {
                     // All events are handled by the `NetworkBehaviourEventProcess`es.
-                    // I.e. the `swarm.next()` future drives the `Swarm` without ever
+                    // I.e. the `swarm.select_next_some()` future drives the `Swarm` without ever
                     // terminating.
-                    match event {
-                        _ => {None}
-                    }
+                    None
                 }
             }
         };
         if let Some((topic, line)) = to_publish {
-            swarm.behaviour_mut().gossipsub.publish(topic.clone(), line.as_bytes()).unwrap();
+            swarm
+                .behaviour_mut()
+                .gossipsub
+                .publish(topic.clone(), line.as_bytes())
+                .unwrap();
         }
-        println!("heyahee");
     }
+}
+
+fn create_gossipsub_behavior(id_keys: Keypair) -> Gossipsub {
+    // To content-address message, we can take the hash of message and use it as an ID.
+    let message_id_fn = |message: &GossipsubMessage| {
+        let mut s = DefaultHasher::new();
+        message.data.hash(&mut s);
+        MessageId::from(s.finish().to_string())
+    };
+
+    // Set a custom gossipsub
+    let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
+        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+        .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+        .message_id_fn(message_id_fn) // content-address messages. No two messages of the
+        .do_px()
+        // same content will be propagated.
+        .build()
+        .expect("Valid config");
+    gossipsub::Gossipsub::new(MessageAuthenticity::Signed(id_keys), gossipsub_config)
+        .expect("Correct configuration")
+}
+
+fn create_kademlia_behavior(
+    local_peer_id: PeerId,
+) -> Kademlia<MemoryStore> {
+    // Create a Kademlia behaviour.
+    let mut cfg = KademliaConfig::default();
+    cfg.set_query_timeout(Duration::from_secs(5 * 60));
+    let store = MemoryStore::new(local_peer_id);
+    Kademlia::with_config(local_peer_id, store, cfg)
+}
+
+fn create_identify_behavior(local_public_key: PublicKey) -> Identify {
+    Identify::new(IdentifyConfig::new("/ipfs/1.0.0".into(), local_public_key))
 }
